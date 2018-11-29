ce1352594952ff20968d36f292e780eb4b976e00
Marie-Carrere <marie.carrere@icloud.com>
2018-11-29 13:45:21 +0100

    social media

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,12 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import profileImage from '../assets/profile.jpeg'
 
+const socialMedias = [
+  { icon: 'fa-github-square', link: 'https://github.com/Marie-Carrere' },
+  { icon: 'fa-linkedin', link: 'https://www.linkedin.com/in/marie-carrere' },
+  { icon: ' fas fa-external-link-square-alt', link: 'https://www.codewars.com/users/Marie_cm' },
+];
+
 const IndexPage = () => (
   <Layout>
     <div className="container">
@@ -12,6 +18,19 @@ const IndexPage = () => (
       </figure>
       <h1 className="title has-text-centered">Marie Carrere</h1>
       <h2 className="subtitle has-text-centered">Full Stack Developer</h2>
+    </div>
+    <div className="has-text-centered">
+      {socialMedias.map(socialMedia => (
+        <a 
+          href={socialMedia.link} 
+          key={socialMedia.icon} 
+          aria-label={socialMedia.icon}
+        >
+          <span className="icon is-large">
+            <i className={`fab fa-2x ${socialMedia.icon}`} />
+          </span>
+        </a>
+      ))}
     </div>
     <section className="section is-size-4 is-size-5-touch">
       <div className="container content">
